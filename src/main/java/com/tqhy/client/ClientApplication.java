d35ad82fea28f9fd6c5fa1b3bdd7c5088e58f486
zhenxun815 <guoyiheng89@gmail.com>
2019-12-17 18:13:09 +0800

    optimize

--- src/main/java/com/tqhy/client/ClientApplication.java
+++ src/main/java/com/tqhy/client/ClientApplication.java
@@ -24,8 +24,6 @@ import org.springframework.context.ConfigurableApplicationContext;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.URL;
 import java.util.logging.Level;
@@ -41,11 +39,11 @@ public class ClientApplication extends Application {
     public static ConfigurableApplicationContext springContext;
     public static Stage stage;
     public static Stage menuStage;
-    public static Stage chooseModelStage;
     public static Stage snapshotStage;
     static Logger logger = LoggerFactory.getLogger(ClientApplication.class);
 
     public static void main(String[] args) {
+        System.setProperty("java.awt.headless", "false");
         String appId = "TQHY_AIA_CLIENT";
         boolean alreadyRunning;
         try {
@@ -65,48 +63,50 @@ public class ClientApplication extends Application {
         } else {
             launch(args);
         }
-
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-
-        stage = primaryStage;
-        initPrimaryStage(stage);
-        stage.setOnCloseRequest(event -> {
-            event.consume();
-            FXMLUtils.loadPopWindow("/static/fxml/warning_onclose.fxml");
-        });
+        initPrimaryStage(primaryStage);
     }
 
     /**
      * 初始化主窗口
      *
-     * @param primaryStage
      * @throws IOException
      */
-    private void initPrimaryStage(Stage primaryStage) throws IOException {
+    private void initPrimaryStage(Stage primaryStage) {
+        logger.info("into initPrimaryStage...");
+        stage = primaryStage;
         Platform.setImplicitExit(false);
-        Parent root = FXMLLoader.load(getClass().getResource("/float/float.fxml"));
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setAlwaysOnTop(true);
-        primaryStage.getIcons()
-                    .add(new javafx.scene.image.Image(
-                            getClass().getResourceAsStream("/deploy/package/windows/logo_title.png")));
+        Parent root = null;
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(ClientApplication.class.getResource("/static/float/float.fxml"));
+            fxmlLoader.setControllerFactory(springContext::getBean);
+            root = fxmlLoader.load();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        logger.info("load float complete...");
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setAlwaysOnTop(true);
+//        primaryStage.getIcons()
+//                    .add(new javafx.scene.image.Image(
+//                            getClass().getResourceAsStream("/deploy/package/windows/logo_title.png")));
         javafx.scene.shape.Rectangle rect = new Rectangle(50, 50);
         rect.setArcHeight(25);
         rect.setArcWidth(25);
         root.setClip(rect);
-
+        logger.info("rect...");
         Scene scene = new Scene(root, 50, 50);
         scene.setFill(Color.TRANSPARENT);
-        primaryStage.setScene(scene);
-        primaryStage.setX(ViewsUtils.getMaxX(50 * 2));
-        primaryStage.setY(ViewsUtils.getScreenHeight() / 2);
-        primaryStage.show();
+        stage.setScene(scene);
+        stage.setX(ViewsUtils.getMaxX(50 * 2));
+        stage.setY(ViewsUtils.getScreenHeight() / 2);
+        stage.show();
 
-        primaryStage.setOnCloseRequest(event -> {
-            logger.info("onclose request...");
+        stage.setOnCloseRequest(event -> {
             event.consume();
             FXMLUtils.loadPopWindow("/static/fxml/warning_onclose.fxml");
         });
@@ -127,49 +127,13 @@ public class ClientApplication extends Application {
             //PopupMenu popupMenu = createPopMenu(stage);
 
             SystemTray systemTray = SystemTray.getSystemTray();
-            String iconPath = ClientApplication.class.getResource("/static/img/logo_systray.png").toExternalForm();
+            String iconPath = ClientApplication.class.getResource("/static/img/AIAlogo.png").toExternalForm();
             URL imageLoc = new URL(iconPath);
             java.awt.Image image = ImageIO.read(imageLoc);
             //final TrayIcon trayIcon = new TrayIcon(image, "打开悬浮窗",popupMenu);
             final TrayIcon trayIcon = new TrayIcon(image);
 
-            trayIcon.addMouseListener(new MouseListener() {
-                @Override
-                public void mouseClicked(MouseEvent e) {
-                    if (e.getButton() > 1) {
-                        logger.info("button 2 clicked...");
-                        Platform.runLater(
-                                () -> FXMLUtils.loadMenu("/static/fxml/menu.fxml",
-                                                         e.getX() + 0D,
-                                                         e.getY() + 0D,
-                                                         60,
-                                                         100));
-                    }
-                }
-
-                @Override
-                public void mousePressed(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseReleased(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseEntered(MouseEvent e) {
-
-                }
-
-                @Override
-                public void mouseExited(MouseEvent e) {
-                    Platform.runLater(() -> menuStage.hide());
-                }
-            });
             systemTray.add(trayIcon);
-
-
         } catch (IOException e) {
             e.printStackTrace();
         } catch (AWTException e) {
@@ -180,6 +144,8 @@ public class ClientApplication extends Application {
     @Override
     public void init() throws Exception {
         super.init();
+        logger.info("into init...");
+
         springContext = SpringApplication.run(ClientApplication.class);
         java.util.logging.Logger globalScreenLogger = java.util.logging.Logger.getLogger(
                 GlobalScreen.class.getPackage().getName());
@@ -193,7 +159,7 @@ public class ClientApplication extends Application {
         }
 
         GlobalScreen.addNativeKeyListener(new GlobalKeyListener());
-        initSystemTray();
+        //initSystemTray();
     }
 
     @Override
